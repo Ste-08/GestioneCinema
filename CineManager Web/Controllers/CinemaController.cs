62c5f6a5cb30e49f4c4bc2ff2ea6e4943c92f7ab
Stefano Cozzula <STEFANO.COZZULA@local.ittcesena.it>
2026-01-28 09:47:58 +0100

    Prima versione del progetto

---
+++ CineManager Web/Controllers/CinemaController.cs
@@ -0,0 +1,35 @@
+﻿using CineManager_Web.Models; // Assicurati che il namespace sia corretto
+using CineManagerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CineManager_Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        // La lista statica dei dati
+        private static List<Sala> _sale = new List<Sala> {
+            new Sala { Id = 1, Film = "Il Glossario", PostiLiberi = 20, TotalePosti = 20, NumeroSala = 1},
+            new Sala { Id = 2, Film = "C# Revenge", PostiLiberi = 5, TotalePosti = 5, NumeroSala = 2}
+        };
+
+        public IActionResult Index()
+        {
+            return View(_sale); // Passa i dati alla Vista
+        }
+
+        public IActionResult Prenota(int id)
+        {
+            var sala = _sale.Find(s => s.Id == id);
+
+            // Se la sala esiste e ci sono posti, occupa il posto
+            if (sala != null && sala.PostiLiberi > 0)
+            {
+                sala.OccupaPosto(); // Assumo che questo metodo diminuisca i PostiLiberi
+            }
+
+            // IMPORTANTE: Dopo la prenotazione, torna all'Index per mostrare i dati aggiornati
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CineManager Web/Models/Sala.cs
@@ -0,0 +1,27 @@
+﻿namespace CineManagerWeb.Models
+{
+    public class Sala
+    {
+        public int Id { get; set; }
+        public string Film { get; set; }
+        public int PostiLiberi { get; set; }
+
+        public int TotalePosti { get; set; }
+
+        public int PostiOccupati
+        {
+            get { return TotalePosti - PostiLiberi; }
+        }
+        public int NumeroSala { get; set; }
+
+
+        public void OccupaPosto()
+        {
+            if (PostiLiberi > 0) PostiLiberi--;
+        }
+
+ 
+
+       
+   }
+}
